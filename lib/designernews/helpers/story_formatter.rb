1ea58516d87b6c936625469624831b5c756cd41a
Cesar Figueroa <cesar.figueroa@me.com>
2014-03-14 20:12:47 -0700

    Add badge hashtags to tweets

--- lib/designernews/helpers/story_formatter.rb
+++ lib/designernews/helpers/story_formatter.rb
@@ -5,7 +5,19 @@ module DesignerNews
     MAX_TWEET_LENGTH = 140 - SHORT_URL_LENGTH_HTTPS - WHITESPACE
 
     def tweet
-      [title.truncate(MAX_TWEET_LENGTH), url].map(&:strip).join(' ')
+      [title_with_badge.truncate(MAX_TWEET_LENGTH), url].join(' ')
+    end
+
+    def title_with_badge
+      if badge
+        [title.strip, hashtag].join(' ')
+      else
+        title.strip
+      end
+    end
+
+    def hashtag
+      badge.sub('_', '').prepend('#')
     end
   end
 end
