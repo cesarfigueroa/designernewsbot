35f3f2f7625a285a397ae63ee44c7c148e9cc58d
Cesar Figueroa <cesar.figueroa@me.com>
2014-05-18 12:30:07 -0700

    Ensure String#truncate accounts for the length of the ellipsis

--- lib/designernews/core_ext/string.rb
+++ lib/designernews/core_ext/string.rb
@@ -1,7 +1,7 @@
 class String
   def truncate(position)
     if self.length > position
-      self.slice(0, position).concat('…')
+      self.slice(0, (position - 1)).concat('…')
     else
       self
     end
